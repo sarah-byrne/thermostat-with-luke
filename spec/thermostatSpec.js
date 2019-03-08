9ce579696d320e644824e0bd485ca6a5d2cd882f
anu <anu_iv2002@hotmail.com>
2019-03-08 14:04:13 +0000

    thermostat basic specifications

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -2,13 +2,110 @@ describe('Thermostat', function() {
   var thermo;
 
   beforeEach(function(){
-    thermo = new Thermostat;
+    thermo = new Thermostat();
   });
-  
+
   describe('Thermostat', function(){
   it('has a starting temperature 20', function(){
-    var thermo = new Thermostat;
     expect(thermo._temp).toEqual(20);
   });
+
+  it('has a minimum temperature 10', function(){
+    expect(thermo._minTemp).toEqual(10);
+  });
+
+  it('has a maximum temperature 25', function(){
+    expect(thermo._maxTemp).toEqual(25);
+  });
 });
+  describe('Thermostat', function(){
+    it('new thermostat with default value true', function(){
+      thermo1 = new Thermostat();
+      expect(thermo._powerSaving).toBe(true);
+    });
+  });
+
+  describe('Thermostat', function(){
+    it('can have a function to increase temperature', function(){
+      thermo.increase(10);
+      expect(thermo._temp).toBeGreaterThan(20);
+    });
+  });
+
+  describe('Thermostat', function(){
+    it('can have a function to decrease temperature', function(){
+      thermo.decrease(10);
+      expect(thermo._temp).toBeLessThan(20);
+    })
+  });
+
+  describe('Thermostat', function(){
+    it('has a power saving mode', function(){
+      expect(thermo._powerSaving).toBe(true);
+      });
+  });
+
+  describe('Thermostat', function(){
+    it('has a on mode', function(){
+      thermo.modeOn();
+      expect(thermo._powerSaving).toBe(true);
+    });
+  });
+
+  describe('Thermostat', function(){
+    it('has a off mode', function(){
+      thermo.modeOff();
+      expect(thermo._powerSaving).toBe(false);
+    });
+  });
+
+  describe('Thermostat', function(){
+    it('has a maximum temperature when mode on', function(){
+      thermo.modeOn();
+      expect(thermo._maxTemp).toBe(25);
+    });
+  });
+
+  describe('Thermostat', function(){
+    it('has a maximum temperature when mode off', function(){
+      thermo.modeOff();
+      expect(thermo._maxTemp).toBe(32);
+    });
+  });
+
+  describe('Thermostat', function(){
+    it('can reset a temperature to 20 with reset function', function(){
+      thermo.reset();
+      expect(thermo._temp).toEqual(20);
+    });
+  });
+
+  describe('Thermostat', function(){
+    it('can tell low usage', function(){
+      thermo.decrease(3);
+      expect(thermo.usage()).toEqual('low usage');
+    });
+
+  });
+
+  describe('Thermostat', function(){
+    it('can tell medium usage', function(){
+      thermo.increase(5);
+      expect(thermo.usage()).toEqual('medium usage');
+    });
+  });
+
+  describe('Thermostat', function(){
+    it('can tell high usage', function(){
+      thermo.increase(6);
+      expect(thermo.usage()).toEqual('high usage');
+    });
+  });
+
+  // describe('Thermostat', function(){
+  //   it('checks the condition for setMaxTemp' function(){
+  //
+  //
+  //   });
+  // });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,4 +1,45 @@
-function Thermostat(){
-   this._temp = 20;
+function Thermostat() {
 
+   this._temp = 20;
+   this._minTemp = 10;
+   this._powerSaving = true;
+   this._maxTemp = 25;
+   this._lowUsage = 18;
+
+};
+Thermostat.prototype.increase = function (value) {
+  this._temp = this._temp + value;
+};
+
+Thermostat.prototype.decrease = function (value) {
+  this._temp = this._temp - value;
+};
+Thermostat.prototype.modeOn = function () {
+  this._powerSaving = true;
+  this.setMaxTemp();
+};
+Thermostat.prototype.modeOff = function(){
+  this._powerSaving = false;
+  this.setMaxTemp();
+};
+Thermostat.prototype.setMaxTemp = function () {
+  if (this._powerSaving == false) {
+    this._maxTemp = 32;
+  } else {
+    this._maxTemp = 25;
+  }
+
+};
+Thermostat.prototype.reset = function(){
+  this._temp = 20;
+};
+
+Thermostat.prototype.usage = function () {
+  if (this._temp < 18) {
+    return "low usage";
+  } else if (this._temp >18 && this._temp < 25) {
+    return "medium usage";
+  } else {
+    return "high usage";
+  }
 };
